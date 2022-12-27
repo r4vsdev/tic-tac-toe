8847003f8201fd877127754739251d0baf2131c9
r4vsdev <r4vsdev@gmail.com>
2022-12-27 14:58:33 -0300

    Fix bugs on the marking method

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -10,18 +10,23 @@ require "matrix"
 class Board
   attr_accessor :first_line, :second_line, :third_line  #, :first_row, :second_row, :third_row
   def initialize
-    @first_line  = [1, 2, 3]
-    @second_line = [4, 5, 6]
-    @third_line  = [7, 8, 9]
+    @first_line  = ["1", "2", "3"]
+    @second_line = ["4", "5", "6"]
+    @third_line  = ["7", "8", "9"]
+    @game_over = false
+  end
+
+  def print
+    p first_line, second_line, third_line
   end
 
   def get_player_move
     puts 'Choose from 1-9 to make your move'
-    @player_move = gets.chomp.to_i
+    @player_move = gets.chomp
   end
 
   def get_CPU_move
-    cpu_move =  rand(1..9)
+    @cpu_move =  rand(1..3).to_s
   end
 
   def mark(move, gamer)
@@ -30,15 +35,43 @@ class Board
       move_index = @first_line.find_index(move)
       @first_line[move_index] = marker
     end
+
+    if @second_line.include?(move)
+      move_index = @second_line.find_index(move)
+      @second_line[move_index] = marker
+    end
+
+    if @third_line.include?(move)
+      move_index = @third_line.find_index(move)
+      @third_line[move_index] = marker
+    end
   end
 
 end
 
 board = Board.new
-p board.first_line, board.second_line, board.third_line
+
+board.print
+
 player_move = board.get_player_move
 board.mark(player_move, 'player')
-p board.first_line
+
+board.print
+
+cpu_move = board.get_CPU_move
+board.mark(cpu_move, 'cpu')
+p cpu_move
+
+board.print
+
+
+# until @game_over do
+  # board.print
+  # player_move = board.get_player_move
+  # board.mark(player_move, 'player')
+  # cpu_move = board.get_CPU_move
+  # board.mark(cpu_move, 'cpu')
+# end
 
 # @first_row = [@first_line[0], @second_line[0], @third_line[0]]
 # @second_row = [@first_line[1], @second_line[1], @third_line[1]]
