0180fc0d4ea3a59f36fcc8d21b27932495536725
r4vsdev <r4vsdev@gmail.com>
2023-01-01 23:16:59 -0300

    Add move tracking to avoid duplicate moves

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,18 +1,12 @@
-require "matrix"
-
-# inicia o board
-#   printa na tela as  3 linhas separadas
-# pega a jogada do player
-# marca no board
-# pega jogada da cpu
-# marca no board
-
 class Board
-  attr_accessor :first_line, :second_line, :third_line  #, :first_row, :second_row, :third_row
+  attr_accessor :available
+  attr_reader :first_line, :second_line, :third_line
+
   def initialize
     @first_line  = ["1", "2", "3"]
     @second_line = ["4", "5", "6"]
     @third_line  = ["7", "8", "9"]
+    @available = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     @game_over = false
   end
 
@@ -23,10 +17,20 @@ class Board
   def get_player_move
     puts 'Choose from 1-9 to make your move'
     @player_move = gets.chomp
+
+    if @available.include?(@player_move)
+      @available.delete(@player_move)
+      return @player_move
+    else
+      puts "The position #{@player_move} is already marked, choose another"
+      get_player_move
+    end
   end
 
   def get_CPU_move
-    @cpu_move =  rand(1..3).to_s
+    @cpu_move = @available.sample
+    @available.delete(@cpu_move)
+    return @cpu_move
   end
 
   def mark(move, gamer)
@@ -50,28 +54,18 @@ class Board
 end
 
 board = Board.new
+# p board.available
 
-board.print
-
-player_move = board.get_player_move
-board.mark(player_move, 'player')
-
-board.print
-
-cpu_move = board.get_CPU_move
-board.mark(cpu_move, 'cpu')
-p cpu_move
-
-board.print
-
-
-# until @game_over do
-  # board.print
-  # player_move = board.get_player_move
-  # board.mark(player_move, 'player')
-  # cpu_move = board.get_CPU_move
-  # board.mark(cpu_move, 'cpu')
-# end
+until @game_over do
+  board.print
+  player_move = board.get_player_move
+  puts "available = #{board.available}"
+  board.mark(player_move, 'player')
+  cpu_move = board.get_CPU_move
+  p cpu_move
+  board.mark(cpu_move, 'cpu')
+  puts "available = #{board.available}"
+end
 
 # @first_row = [@first_line[0], @second_line[0], @third_line[0]]
 # @second_row = [@first_line[1], @second_line[1], @third_line[1]]
